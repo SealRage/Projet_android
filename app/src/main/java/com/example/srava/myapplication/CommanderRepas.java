119b178bd083281afe73285fe71daf76e5330d37
nocentic <nocenti.cedric@gmail.com>
2016-03-22 17:40:56 +0100

    bdd encore

--- app/src/main/java/com/example/srava/myapplication/CommanderRepas.java
+++ app/src/main/java/com/example/srava/myapplication/CommanderRepas.java
@@ -3,6 +3,7 @@ package com.example.srava.myapplication;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.app.Activity;
 import android.content.Context;
@@ -17,10 +18,14 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import com.example.srava.myapplication.Database.Menu;
+import com.example.srava.myapplication.Database.MenuAdapter;
+import com.example.srava.myapplication.Database.MenuHelper;
 import com.example.srava.myapplication.Database.Produit;
+import com.example.srava.myapplication.Database.TypeAdapter;
 
 import org.w3c.dom.Text;
 
@@ -35,6 +40,9 @@ public class CommanderRepas extends android.support.v4.app.Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     protected  Menu myMenu ;
+    MenuAdapter sauvegardeMenuDB;
+    TypeAdapter insertTypeDB;
+    MenuAdapter insertMenuDB;
     //Get Global Controller Class object (see application tag in AndroidManifest.xml)
     final Controller aController = (Controller) getContext();
 
@@ -95,6 +103,25 @@ public class CommanderRepas extends android.support.v4.app.Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        insertTypeDB = new TypeAdapter( getActivity().getApplicationContext());
+        insertTypeDB.open();
+        insertTypeDB.insertType("Salut");
+        insertTypeDB.close();
+
+
+
+
+
+
+        sauvegardeMenuDB = new MenuAdapter( getActivity().getApplicationContext());
+        sauvegardeMenuDB.open();
+        sauvegardeMenuDB.insertMenu("Groumand", 1, 3, new ArrayList<Produit>());
+        sauvegardeMenuDB.insertMenu("Plus",1,4, new ArrayList<Produit>());
+        sauvegardeMenuDB.insertMenu("Classique",1,5, new ArrayList<Produit>());
+        Log.wtf("Open1", "Open");
+        populate();
+        ((ListView)view.findViewById(R.id.ListView_Menu)).setAdapter(populate());
+        sauvegardeMenuDB.close();
 
         final Controller aController = (Controller) getActivity().getApplicationContext();
         // Récupération de la listeview
@@ -197,5 +224,17 @@ public class CommanderRepas extends android.support.v4.app.Fragment {
     public void onSaveInstanceState(Bundle bundle) {
         super.onSaveInstanceState(bundle);
         // store data in the bundle
+    }
+
+
+
+    private ListAdapter populate(){
+        ListAdapter adapter = new SimpleCursorAdapter( getActivity().getApplicationContext(),
+                R.layout.fragment_commander_repas, sauvegardeMenuDB.getAllData(),
+                new String[] {MenuHelper.KEY_NAME, MenuHelper.KEY_PRIX},
+                new int[] {R.id.TextAppTitle, R.id.TextAppSummary});
+        // Bind to our new adapter.
+        return adapter;
+
     }
 }
